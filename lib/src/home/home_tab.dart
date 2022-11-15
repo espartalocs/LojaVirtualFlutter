363bb3cd3b684a5e2649c6396a0997ae2b396575
espartalocs <kleber.sonlima@hotmail.com>
2022-11-15 14:33:09 -0300

    Adicionado campo pesquisa a home_tab

--- lib/src/home/home_tab.dart
+++ lib/src/home/home_tab.dart
@@ -47,10 +47,27 @@ class HomeTab extends StatelessWidget {
       ),
       body: Column(
         children: [
-          TextFormField(
-            decoration: const InputDecoration(
-              filled: true,
-              fillColor: Colors.white,
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20, vertical: 10
+            ),
+            child: TextFormField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                isDense: true,
+                hintText: 'Pesquisar',
+                hintStyle: TextStyle(
+                  color: Colors.grey.shade400,
+                  fontSize: 14,
+                ),
+                prefixIcon: Icon(Icons.search,
+                    color: CustomColors.customConstrastColor, size: 21),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(60),
+                    borderSide:
+                        const BorderSide(width: 0, style: BorderStyle.none)),
+              ),
             ),
           )
         ],
